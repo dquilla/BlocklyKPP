48b09d535f1e5e3023ecd17d870705f1bc52267d
dquilla <davichoq@gmail.com>
2015-11-15 18:05:33 -0600

    Variable declaration in functions, ifs and loops

--- blockly-k++ - Copy/blocks_compressed.js
+++ blockly-k++ - Copy/blocks_compressed.js
@@ -778,6 +778,8 @@ Blockly.Blocks.varassignment = {
           .appendField("set")
           .appendField(new Blockly.FieldTextInput("varName"), "nombreVar")
           .appendField("to");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
       this.setColour(330);
       this.setTooltip('');
       this.setHelpUrl('http://www.example.com/');
